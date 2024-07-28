3cde9c4f71194373f5f99ae7137c1bfa2abff303
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2024-07-28 11:24:01 +0800

    Update omni_sl8541e_1h10.mk

--- omni_sl8541e_1h10.mk
+++ omni_sl8541e_1h10.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from sl8541e_1h10 device
-$(call inherit-product, device/SPRD/sl8541e_1h10/device.mk)
+$(call inherit-product, device/xh011/sl8541e_1h10/device.mk)
 
 PRODUCT_DEVICE := sl8541e_1h10
 PRODUCT_NAME := omni_sl8541e_1h10
